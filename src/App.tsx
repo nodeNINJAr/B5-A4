b0252f7e78f6d39c701d9589aba08f6a32c7598d
Mehedi hasan <nodeNINJAr@users.noreply.github.com>
2025-07-25 19:31:07 +0600

    project-setup-nessery-pakedge-install

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-5xl text-red-900'>App <button className='btn btn-primary'>test</button></div>
+  )
+}
+
+export default App
